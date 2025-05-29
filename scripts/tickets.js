a1842e333ca02cc35c889f391befaf7c2f506dbe
tandra048 <2048tandraju@gmail.com>
2025-05-29 23:56:33 +0600

    button disable  done

--- scripts/tickets.js
+++ scripts/tickets.js
@@ -5,3 +5,14 @@ document.getElementById('paribahan').scrollIntoView({behavior:"smooth"});
 }
 
 
+// function byClick(){
+// setButtonColorOnClick('seat','#1DD100');
+
+// } 
+
+function byClick(){
+   
+    }
+    
+    
+    
---
+++ scripts/utility.js
@@ -0,0 +1,14 @@
+// function setButtonColorOnClick(className, color) {
+//   const buttons = document.querySelectorAll('.' + className);
+//  const totalSeatValue = document.getElementById('total-seat');
+//  const totalSeat = totalSeatValue.innerText;
+//  console.log(totalSeat);
+//   buttons.forEach(button => {
+//     button.addEventListener('click', function () {
+    
+//     button.style.backgroundColor = color;
+   
+//     });
+//   });
+// }
+
